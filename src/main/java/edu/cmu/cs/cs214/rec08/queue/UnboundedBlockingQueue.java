074fb52ae28d4ff314713506d1f7b4e4e564bcb0
Juel-Le <lezhuer@outlook.com>
2024-10-25 01:00:41 -0400

    completed queue

--- src/main/java/edu/cmu/cs/cs214/rec08/queue/UnboundedBlockingQueue.java
+++ src/main/java/edu/cmu/cs/cs214/rec08/queue/UnboundedBlockingQueue.java
@@ -16,20 +16,32 @@ public class UnboundedBlockingQueue<E> implements SimpleQueue<E> {
 
     public UnboundedBlockingQueue() { }
 
-    public boolean isEmpty() { return queue.isEmpty(); }
+    public synchronized boolean isEmpty() { return queue.isEmpty(); }
 
-    public int size() { return queue.size(); }
+    public synchronized int size() { return queue.size(); }
 
-    public E peek() { return queue.peek(); }
+    public synchronized E peek() { return queue.peek(); }
 
-    public void enqueue(E element) { queue.add(element); }
+    public synchronized void enqueue(E element) { 
+        queue.add(element); 
+        notifyAll();
+    }
 
     /**
      * TODO:  Change this method to block (waiting for an enqueue) rather
      * than throw an exception.  Completing this task may require
      * modifying other methods.
      */
-    public E dequeue() { return queue.remove(); }
+    public synchronized E dequeue() { 
+        while (queue.isEmpty()) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            } 
+        }
+        return queue.remove(); 
+    }
 
-    public String toString() { return queue.toString(); }
+    public synchronized String toString() { return queue.toString(); }
 }
